6b77b1e2c56c6c18da66adb9dfabe0b03384b79e
pavsenin <pavel.avsenin@devexpress.com>
2020-12-02 10:45:29 +0300

    Run publish.py for deploy

--- publish.py
+++ publish.py
@@ -1,4 +1,7 @@
+from utils import Log
 from config_provider import config
 
+log = Log(config, 'publish')
 for python_file in ['config', 'utils', 'db', 'constants', 'telegram', 'preprocessing', 'scraping', 'updating', 'update']:
-    config.OS.copy_file(config.to_abs_path('src/aiodds', f'{python_file}.py'), config.to_abs_path('prod', f'{python_file}.py'))
+    result = config.OS.copy_file(config.to_abs_path('src/aiodds', f'{python_file}.py'), config.to_abs_path('prod', f'{python_file}.py'))
+    log.debug(f'Copy {python_file} with {result}')
