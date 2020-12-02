f3e2f2635811c193bb96714eb895284d0b2ca41d
pavsenin <pavel.avsenin@devexpress.com>
2020-12-02 11:49:45 +0300

    Try to fix deploy stage

--- publish.py
+++ publish.py
@@ -3,5 +3,5 @@ from config_provider import config
 
 log = Log(config, 'publish')
 for python_file in ['config', 'utils', 'db', 'constants', 'telegram', 'preprocessing', 'scraping', 'updating', 'update']:
-    result = config.OS.copy_file(config.to_abs_path('src/aiodds', f'{python_file}.py'), config.to_abs_path('prod', f'{python_file}.py'))
+    result = config.OS.copy_file(f'{python_file}.py', config.to_abs_path('prod', f'{python_file}.py'))
     log.debug(f'Copy {python_file} with {result}')
