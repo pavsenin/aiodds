03b1bd2541671490afd16b479d649b1122fe26b0
pavsenin <pavel.avsenin@devexpress.com>
2020-12-02 13:21:54 +0300

    Fix publishing

--- publish_bot.py
+++ publish_bot.py
@@ -1,10 +1,17 @@
 import os, sys
 
 from telegram import Telegram
+from publish import log
 
 bot = Telegram()
+std_input = None
+if not sys.stdin.isatty():
+    std_input = sys.stdin.readlines()
 link = bot.link('deployment', f"https://gitlab.com/pavsenin/aiodds/-/pipelines/{os.environ['CI_PIPELINE_ID']}")
-if not sys.stdin:
-    bot.send_message(f"AIOdds {link} was successful")
+if not std_input:
+    message = f"AIOdds {link} was successful"
 else:
-    bot.send_message(f"AIOdds {link} failed: {sys.stdin}")
+    message = f"AIOdds {link} failed: {std_input}"
+
+log.debug(message)
+bot.send_message(message)
