e9f24f712acf374bacf908882cd8f735d8b166fa
pavsenin <pavel.avsenin@devexpress.com>
2020-12-02 12:11:09 +0300

    Send bot message after deploy

--- publish.py
+++ publish.py
@@ -1,7 +1,14 @@
-from utils import Log
+from telegram import Telegram
 from config_provider import config
 
-log = Log(config, 'publish')
+success = True
 for python_file in ['config', 'utils', 'db', 'constants', 'telegram', 'preprocessing', 'scraping', 'updating', 'update']:
     result = config.OS.copy_file(f'{python_file}.py', config.to_abs_path('prod', f'{python_file}.py'))
-    log.debug(f'Copy {python_file} with {result}')
+    success = success and result == b''
+    if not success:
+        break
+bot = Telegram()
+if success:
+    bot.send_message(bot.green('AIOdds deployment was successful'))
+else:
+    bot.send_message(bot.red(f'AIOdds deployment failed: {python_file}'))
--- telegram.py
+++ telegram.py
@@ -6,6 +6,10 @@ class Telegram:
         self.bot_chat_id = '712721440'
         self.url = 'https://api.telegram.org/'
 
+    def color(self, message, color): return f'<span style="color:{color}">{message}</span>'
+    def red(self, message): return self.color(message, 'red')
+    def green(self, message): return self.color(message, 'green')
+
     def send_message(self, message):
         request = f'{self.url}bot{self.bot_token}/sendMessage?chat_id={self.bot_chat_id}&parse_mode=Markdown&text={message}'
         return get(request).json()
