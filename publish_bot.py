539f42192d2d51d9d8fde3a4a8f337168f28f1c8
pavsenin <pavel.avsenin@devexpress.com>
2020-12-02 13:05:21 +0300

    Improve publishing

--- publish_bot.py
+++ publish_bot.py
@@ -1,11 +1,10 @@
-import os
+import os, sys
+
 from telegram import Telegram
 
-publish_results = input()
-print(publish_results)
-url = f"https://gitlab.com/pavsenin/aiodds/-/pipelines/{os.environ['CI_PIPELINE_ID']}"
 bot = Telegram()
-if not publish_results:
-    bot.send_message(f"AIOdds deployment was {bot.link('successful', url)}")
+link = bot.link('deployment', f"https://gitlab.com/pavsenin/aiodds/-/pipelines/{os.environ['CI_PIPELINE_ID']}")
+if not sys.stdin:
+    bot.send_message(f"AIOdds {link} was successful")
 else:
-    bot.send_message(f"AIOdds deployment {bot.link('failed', url)}: {python_file}")
+    bot.send_message(f"AIOdds {link} failed: {sys.stdin}")
